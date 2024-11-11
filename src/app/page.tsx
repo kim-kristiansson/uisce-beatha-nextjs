f0b6223007f66f041f7110cb13443610426adb0b
kim-kristiansson <154594963+kim-kristiansson@users.noreply.github.com>
2024-11-12 00:29:48 +0100

    update home page url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
-import {Button} from '@/components/ui/button'
+import { Button } from "@/components/ui/button"
+import qs from "qs"
 
-async function getStrapiData(path:string) {
-  const baseUrl = "http://127.0.0.1:1337"
+const homePageQuery = qs.stringify({
+    populate: {
+        blocks: {
+            on: {
+                "layout.hero-section": {
+                    populate: {
+                        image: {
+                            fields: ["url", "alternativeText"],
+                        },
+                        link: {
+                            populate: true,
+                        },
+                    },
+                },
+            },
+        },
+    },
+})
 
-  try {
-    const response = await fetch(baseUrl + path)
-    const data = await response.json()
-    return data
-  }
-  catch (error) {
-    console.error(error)
-  }
+async function getStrapiData(path: string) {
+    const baseUrl = "http://127.0.0.1:1337"
+
+    const url = new URL(path, baseUrl)
+    url.search = homePageQuery
+
+    try {
+        const response = await fetch(url.href)
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 export default async function Home() {
-  const strapiData = await getStrapiData("/api/home-page")
-  console.log(strapiData);
-  
-  return (
-    <Button>Button</Button>
-  );
+    const strapiData = await getStrapiData("/api/home-page")
+
+    console.log(strapiData)
+
+    const { title, description } = strapiData.data
+
+    return (
+        <main>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </main>
+    )
 }
